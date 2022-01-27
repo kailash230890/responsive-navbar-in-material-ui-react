be2e4113e6ff1d4913dd289a4494766bce070b76
Kailash <53666209+kailash230890@users.noreply.github.com>
2022-01-27 13:03:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Responsive_Navbar from './Responsive_Navbar';
+// import RecorShow from './RecordShow';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <RecorShow /> */}
+     <Responsive_Navbar />
+    </div>
+  );
+}
+
+export default App;
